f863516441dc6cfbbe54c21bb203bb27664a01f5
Martin Blieninger <martinblieninger2208@gmail.com>
2024-07-18 17:57:36 +0200

    updated some things

---
+++ src/js/app_content.js
@@ -0,0 +1,7 @@
+createAppContent('e7d5e3de-44f4-11ef-b64a-0b627cc83e34', 'msg-encrypt', 'msg-encrypt ist eine App mit der Text (beispielsweise WhatsApp-Nachrichten) in eine verschlüsselte Nachricht verwandelt werden kann.', '5.0.0', 'https://raw.githubusercontent.com/cracky22/AppStore/master/AppIcon/msg.png');
+
+createAppContent('e8682032-44f4-11ef-b221-93373ac0bd56', 'SlideThings', 'SlideThings text', '4.5', 'https://raw.githubusercontent.com/cracky22/AppStore/master/AppIcon/slidethings.png');
+
+createAppContent('e92941fe-44f4-11ef-9246-539e0da4eaa0', 'Notizen', 'Mit Notizen kannst du simple Text-basierte Notizen erstellen, bearbeiten und löschen. Die App verfügt über ein automatisches Cloud-Backup.', '3.0.3', 'https://raw.githubusercontent.com/cracky22/AppStore/master/AppIcon/notes.png');
+
+createAppContent('e977132a-44f4-11ef-9165-7f217aade36d', 'pwgen', 'pwgen oder auch Passwort Generator ist ein Tool, mitdem du dir sichere Passwörter generiern kannst. Dies erfolgt vollständig offline!', '1.3.7', 'https://raw.githubusercontent.com/cracky22/AppStore/master/AppIcon/pwgen.png');
